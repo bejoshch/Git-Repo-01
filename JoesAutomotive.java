5ba5b2e421e88b32b16bd8147f654a2de9470f19
bejoshch <37763232+bejoshch@users.noreply.github.com>
2018-03-30 15:06:53 -0500

    Added location comment of GitDesktop

--- JoesAutomotive.java
+++ JoesAutomotive.java
@@ -4,7 +4,7 @@ import javax.swing.*;    			// Required for Swing classes
 //import javax.swing.border.Border;
 import javax.swing.SwingConstants;
 import java.text.DecimalFormat;		// Required for decimal format class
-
+//C:\Users\TEMP\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 /**
  * JoesAutomotive : A vehicle maintenance and repair cost calculator for Joe's Automotive.<br>
  *
